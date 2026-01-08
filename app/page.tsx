01fccffaeedb501e529b9844f1ac8ceec712c1be
Damian Rodriguez <damian.hr.rodriguez@gmail.com>
2026-01-08 18:46:26 -0300

    Saque algunas cosas que quedamos en comun y  tambien modifique los textos

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from '@/components/Hero'
 import Features from '@/components/Features'
 import Services from '@/components/Services'
 import Technologies from '@/components/Technologies'
-import Testimonials from '@/components/Testimonials'
 import FAQ from '@/components/FAQ'
 import CTA from '@/components/CTA'
 import Footer from '@/components/Footer'
@@ -16,7 +15,6 @@ export default function Home() {
       <Features />
       <Services />
       <Technologies />
-      <Testimonials />
       <FAQ />
       <CTA />
       <Footer />
